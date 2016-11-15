026037eedc7e127198b4cf033f7d8fdc454f97f2
Richard Hurt <rnhurt@gmail.com>
2016-11-15 14:59:50 -0500

    Update EC2 volume backup routine

--- nodejs/RefreshRDS/lib/EC2.js
+++ nodejs/RefreshRDS/lib/EC2.js
@@ -2,71 +2,75 @@
 
 var util = require('util');
 var AWS = require('aws-sdk');
-var sanitize = require('sanitize-filename');
+var slug = require('slug');
 
 module.exports = {
 
   // Create a snapshot of an EC2 volume and clean any 'old' snapshots
   backupVolume: function(region, volumeName, backupsToKeep, callback) {
     var EC2 = new AWS.EC2({apiVersion: '2016-09-15', region: region});
-    var volume = null;
-    var tags = null;
+    var snapshots = null;
 
     // Get the existing snapshots by getting the Volume using its name and then
     // using the volumeId to get all of it's snapshots
     console.log('Getting volume information...');
-    var snapshots = EC2.describeVolumes({
+    EC2.describeVolumes({
       Filters: [{Name: "tag:Name", Values: [volumeName]}]
-    }).promise().then(function(result){
+    }).promise().then(function(result){      
       if(result.Volumes.length < 1){ callback('Volume not found'); return null;}
-      volume = result.Volumes[0];
-      console.log('Getting snapshot information...'); 
-      return EC2.describeSnapshots({
+
+      var volume = result.Volumes[0];
+      var snapshotPromise = EC2.describeSnapshots({
             Filters: [ {Name: "volume-id", Values:[volume.VolumeId]} ]
             }).promise().then(function(result){
-              return { Snapshots: result.Snapshots }; 
+              console.log(' - got snapshot information'); 
+              return result.Snapshots; 
             })
-    });
 
+      var tagPromise = EC2.describeTags({
+        Filters: [{Name: "tag:Name", Values: [volumeName]}, {Name:'resource-type', Values: ['volume']}]
+      }).promise().then(function(result){
+        console.log(' - got existing volume tags'); 
+        return result.Tags; 
+      });
 
-    // Get the tags of the existing volume
-    console.log('Getting tags of existing volume...'); 
-    var tags = EC2.describeTags({
-      Filters: [{Name: "tag:Name", Values: [volumeName]}, {Name:'resource-type', Values: ['volume']}]
-    }).promise().then(function(result){
-      return result.Tags; 
-    });
+      // After all the necessary information is gathered, create the snapshot and clean up any old items
+      Promise.all([snapshotPromise, tagPromise]).then(function(values) {
+        snapshots = values[0];
+        var tags = values[1];
 
-    // After all the necessary information is gathered, create the snapshot and clean up any old items
-    Promise.all([snapshots, tags]).then(function(values) {
-      if (volume == null ) { return null; }
-      var snapshots = values[0];
-      var tags = values[1];
+        // Create the new snapshot
+        var date = new Date().toISOString().replace(/-/g,"").replace(/:/g,"");
+        var description = slug(volumeName+'-snapshot-'+date, {lower:true});
+        console.log("Creating new snapshot: '%s'", description);
+        EC2.createSnapshot({
+          Description: description,
+          VolumeId: volume.VolumeId
+        }).promise().then(function(data){
+          var newTags = tags.map(function(tag){ return {Key: tag.Key, Value: tag.Value}; }); 
+          EC2.createTags({Resources: [data.SnapshotId], Tags: newTags}).promise();
+          return EC2.waitFor('snapshotCompleted', { SnapshotIds: [data.SnapshotId] }).promise();
 
-      // Create the new snapshot
-      var date = new Date().toISOString().replace(/-/g,"").replace(/:/g,"");
-      var description = sanitize(volumeName)+'-snapshot-'+date;
-      var snapshot = EC2.createSnapshot({
-        Description: description,
-        VolumeId: volume.VolumeId
-      }).promise().then(function(data){
-        var newTags = tags.map(function(tag){ return {Key: tag.Key, Value: tag.Value}; }); 
-        EC2.createTags({Resources: [data.SnapshotId], Tags: newTags}).promise();
-        return EC2.waitFor('snapshotCompleted', { SnapshotIds: [data.SnapshotId] }).promise();
+        // Clean up old snapshots
+        }).then(function(data){
+          if(snapshots.length > backupsToKeep){
+            console.log('Removing the oldest snapshot(s)...');
 
-      // Clean up old snapshots
-      }).then(function(data){
-        var oldSnapshots = 1;
-        if(oldSnapshots.length > backupsToKeep){
-          console.log("Removing the oldest snapshot(s)...");
+            var oldSnapshots = snapshots.sort(function(a,b){
+              if(a.SnapshotCreateTime > b.SnapshotCreateTime){return 1;}
+              if(a.SnapshotCreateTime < b.SnapshotCreateTime){return -1;}
+              return 0;
+            }).splice(backupsToKeep, snapshots.length-backupsToKeep);
 
-          // Remove all the old snapshots and don't wait around for results
-          for(var item in oldSnapshots){
-            EC2.deleteSnapshot({SnapshotId:item.snapshotId}).promise();
+            // Remove all the old snapshots and don't wait around for results
+            oldSnapshots.forEach(function(snapshot){
+              console.log(" - delete '%s", snapshot.SnapshotId);
+              EC2.deleteSnapshot({SnapshotId:snapshot.SnapshotId}).promise();
+            });
           }
-        }
-        
-        callback(null);      
+          
+          callback(null);      
+        });
       });
     });
   }
--- nodejs/RefreshRDS/scripts/backupVolume.js
+++ nodejs/RefreshRDS/scripts/backupVolume.js
@@ -4,7 +4,7 @@
 var EC2 = require("../lib/EC2");
 var region = process.argv[2];
 var volumeName = process.argv[3];
-var backupsToKeep = process.argv[4];
+var backupsToKeep = parseInt(process.argv[4]);
 
 EC2.backupVolume(region, volumeName, backupsToKeep, function(err) {
   if (err) { console.error(err); return; }
