741e3fa5c54b9c8f883677b1a631147dd5361a50
Richard Hurt <rnhurt@gmail.com>
2016-11-18 09:10:51 -0500

    Add CLI option processing to backupVolume script

--- nodejs/scripts/backupVolume.js
+++ nodejs/scripts/backupVolume.js
@@ -2,11 +2,22 @@
 "use strict";
 
 var EC2 = require("../lib/EC2");
-var region = process.argv[2];
-var volumeName = process.argv[3];
-var backupsToKeep = parseInt(process.argv[4]);
+var program = require('commander');
 
-EC2.backupVolume(region, volumeName, backupsToKeep, function(err) {
+program
+  .version('0.0.1')
+  .description('Create a snapshot of an AWS EC2 volume')
+  .option('-r, --region <string>', 'AWS Region')
+  .option('-v, --volume <string>', 'Name of the volume to backup')
+  .option('-k, --keep <n>', 'Number of backups to keep', parseInt)
+  .parse(process.argv);
+
+  if(4 != process.argv.slice(4).length){
+    program.outputHelp();
+    process.exit(1);
+  }
+
+EC2.backupVolume(program.region, program.volume, program.keep, function(err) {
   if (err) { console.error(err); return; }
   console.log("Done.");
 });
